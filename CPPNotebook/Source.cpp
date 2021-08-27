17c23073fea7d6b1f4a97a8f025b6a88e6b409a9
Vinayak Dwarkoba Gaikwad <vinayakdwarkobagaikwad@gmail.com>
2021-08-27 12:42:12 +0530

    .

--- CPPNotebook/Source.cpp
+++ CPPNotebook/Source.cpp
@@ -3,6 +3,39 @@
 
 using namespace std;
 
-int main() {
-	
+class myStudent {
+	private:
+		int roll_no;
+		string name;
+		float sgpa;
+    public:
+	    void getData();
+	    void showData();
+};
+
+
+//function impementation outside the class
+void myStudent::getData() {
+	cout << "Enter your name:";
+	cin >> name;
+	cout << "Enter your roll no:";
+	cin >> roll_no;
+	cout << "Enter your SCGPA:";
+	cin >> sgpa;
+};
+
+//function impementation outside the class
+void myStudent::showData() {
+	cout << "Name:" << name;
+	cout << "Roll no:" << roll_no;
+	cout << "SCGPA:" << sgpa;
+};
+
+void main() {
+	myStudent student1;
+	int i = 0;
+	for (i == 0; i < 10; i++) {
+		student1.getData();
+		student1.showData();
+	}
 };
