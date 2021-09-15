ee4a595b7275259d87c10e018244abbbb38edd60
Vinayak Dwarkoba Gaikwad <vinayakdwarkobagaikwad@gmail.com>
2021-09-15 11:55:26 +0530

    .

--- CPPNotebook/SearchingAndSorting.cpp
+++ CPPNotebook/SearchingAndSorting.cpp
@@ -1,35 +1,58 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 
-int searching() {
-	int i;
-	int search_key;
-	
-	int information[10];
+class stu_database {
+private:
+	struct student {
+		int roll_no;
+		string name;
+		float SGPA;
+	} mydatabase[5];
 
-	for (i = 0; i < 10; i++) {
-		cin >> information[i];
-		cout << "\n";
-	}
+public:	
+	void get_data() {
 
-	//linear search
-	cout << "Entre what to search?:";
-	cin >> search_key;
-	
-	int e=0;
-	int flag = 0;
-	
-	while ((e<10) && (flag == 0)) {
-		
-		if (search_key == information[e]) {
-			flag = 1;
-			cout << "Found!  @" << e;
-			break;
-		}
+		for (int i = 0; i <= 4; i++) {
+			cout << "Roll no:";
+			cin >> mydatabase[i].roll_no;
+			cout << "Name:";
+			cin >> mydatabase[i].name;
+			cout << "SGPA";
+			cin >> mydatabase[i].SGPA;
+		};
 
-		e++;
-	}
+	};
+
+	void l_search_rollno(int key) {
+		int i = 0;
+
+		do  {
+			if (key == mydatabase[i].roll_no) {
+				cout << "Found! \n";
+				cout << mydatabase[i].roll_no <<"\n";
+				cout << mydatabase[i].name << "\n";
+				cout << mydatabase[i].SGPA << "\n";
+				break;
+			}
+			i++;
+		} while ((mydatabase[i].roll_no != key));
+
+	};
+
+};
+
+int main() {
+	
+	stu_database List1;
+	List1.get_data();
+
+	int key;
+	cout << "What to search:";
+	cin >> key;
+	List1.l_search_rollno(key);
+
 	
 	return 0;
 		
